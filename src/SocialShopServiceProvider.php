46efe0c1ffe3bd231e9852738f8a6990970a7d98
Abul Hassan <imabulhasan99@gmail.com>
2025-08-27 15:05:28 +0600

    configure package service provider to publish asset, migration,view and merge config

--- src/SocialShopServiceProvider.php
+++ src/SocialShopServiceProvider.php
@@ -19,7 +19,28 @@ class SocialShopServiceProvider extends PackageServiceProvider
             ->name('social-shop')
             ->hasConfigFile()
             ->hasViews()
-            ->hasMigration('create_social_shop_table')
+            ->hasMigration([
+            'create_social_shop_table'
+            ])
             ->hasCommand(SocialShopCommand::class);
+    }
+    
+    public function packageBooted(): void
+    {
+        $this->publishes([
+            __DIR__ . '/../config/social-shop.php' => config_path('social-shop.php'),
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/social-shop'),
+            __DIR__ . '/../database/migrations' => database_path('migrations'),
+        ], 'social-shop');
+
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'social-shop');
+        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'social-shop');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+    }
+
+    public function packageRegistered(): void
+    {
+       $this->mergeConfigFrom(__DIR__ . '/../config/social-shop.php', 'social-shop');
     }
 }
