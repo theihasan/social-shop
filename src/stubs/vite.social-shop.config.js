5d60a1472004f496a30d1b70d61197488133e902
Abul Hassan <imabulhasan99@gmail.com>
2025-08-27 16:47:49 +0600

    add vite config stubs for this package

--- src/stubs/vite.social-shop.config.js
+++ src/stubs/vite.social-shop.config.js
@@ -5,10 +5,18 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/vendor/social-shop/app.js'],
+            input: [
+                'resources/js/vendor/social-shop/app.js',
+                'resources/css/vendor/social-shop/app.css'
+            ],
             refresh: true,
             buildDirectory: 'build/social-shop'
         }),
         vue(),
     ],
+    resolve: {
+        alias: {
+            '@': '/resources/js/vendor/social-shop',
+        },
+    },
 })
