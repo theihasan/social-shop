20ed0a2d90fa8e75f87c1ac7127d045dd0c8974a
Abul Hassan <imabulhasan99@gmail.com>
2025-08-27 16:47:06 +0600

    update app blade

--- resources/app.blade.php
+++ resources/app.blade.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" class="h-full">
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite('resources/js/vendor/social-shop/app.js')
+    <title>{{ $page['props']['title'] ?? 'Social Shop' }}</title>
+    
+    @vite(['resources/css/vendor/social-shop/app.css', 'resources/js/vendor/social-shop/app.js'])
     @inertiaHead
 </head>
-<body>
+<body class="h-full bg-gray-50 font-inter antialiased">
     @inertia
 </body>
 </html>
