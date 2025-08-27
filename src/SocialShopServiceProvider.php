11e5526d2dc074d271544051094dfd0672bfd09e
theihasan <142471724+theihasan@users.noreply.github.com>
2025-08-27 09:05:51 +0000

    Fix styling

--- src/SocialShopServiceProvider.php
+++ src/SocialShopServiceProvider.php
@@ -20,27 +20,27 @@ class SocialShopServiceProvider extends PackageServiceProvider
             ->hasConfigFile()
             ->hasViews()
             ->hasMigration([
-            'create_social_shop_table'
+                'create_social_shop_table',
             ])
             ->hasCommand(SocialShopCommand::class);
     }
-    
+
     public function packageBooted(): void
     {
         $this->publishes([
-            __DIR__ . '/../config/social-shop.php' => config_path('social-shop.php'),
-            __DIR__ . '/../resources/views' => resource_path('views/vendor/social-shop'),
-            __DIR__ . '/../database/migrations' => database_path('migrations'),
+            __DIR__.'/../config/social-shop.php' => config_path('social-shop.php'),
+            __DIR__.'/../resources/views' => resource_path('views/vendor/social-shop'),
+            __DIR__.'/../database/migrations' => database_path('migrations'),
         ], 'social-shop');
 
-        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'social-shop');
-        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'social-shop');
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'social-shop');
+        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'social-shop');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     }
 
     public function packageRegistered(): void
     {
-       $this->mergeConfigFrom(__DIR__ . '/../config/social-shop.php', 'social-shop');
+        $this->mergeConfigFrom(__DIR__.'/../config/social-shop.php', 'social-shop');
     }
 }
